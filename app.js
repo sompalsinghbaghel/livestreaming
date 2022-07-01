e4aab32fbeef99bf2684aeb43ac16c0e5df9ea8f
SomPal Singh Baghel <78479619+sompalsinghbaghel@users.noreply.github.com>
2022-07-01 14:48:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const spawn = require('child_process').spawn;
 
-const keyRTMP = process.env.KEYRTMP //You Can Change the RTMP url as per your need. For Ex: Telegram, YouTube, Twitch etc.
+const keyRTMP = "rtmp://ingest.mediaserver.express/live/qcewKnba?pwd=efaaaa"
 
-const urlSrc = process.env.URLSRC // Your Video File you want To loop through.
+const urlSrc = "https://epiconvh.s.llnwi.net/showbox/master_1024.m3u8"
 
 let ffmpeg = spawn('ffmpeg', ['-re', '-stream_loop', '-1', '-i', `${ urlSrc }`, '-c:v', 'libx264', '-preset', 'veryfast',
 	'-framerate', '60', '-maxrate', '3000k', '-bufsize', '6000k', '-pix_fmt', 'yuv420p', '-g', '50', '-c:a', 'aac', '-b:a', '128k',
